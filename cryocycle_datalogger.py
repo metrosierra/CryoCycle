c2f2ef5cc0e9d971e1939aa6a69c41eff72c76fd
metrosierra <mingsong.wu@unige.ch>
2025-09-11 00:53:14 +0200

    working datalogger showing live temp data from ctc100, next is macro, then gui

--- cryocycle_datalogger.py
+++ cryocycle_datalogger.py
@@ -1,8 +1,9 @@
 import sys
 import os
 import numpy as np
+import json
 
-config_relative_path = '/'
+config_relative_path = 'config/'
 drivers_relative_path = '/'
 sys.path.insert(1, os.path.join(sys.path[0], drivers_relative_path)) ### TO POINT TO DEVICE INTERFACING FOLDER
 abs_path = os.path.dirname(os.path.abspath(__file__))
@@ -11,3 +12,89 @@ if abs_path not in sys.path:
 
 from drivers.tempcontroller_ctc100 import TempControl_CTC100
 from drivers.liveplotter_heavy import LivePlotAgent 
+
+
+class CryoCycler:
+
+    def __init__(self, config_dir = config_relative_path):
+
+        self.config_dir = config_dir
+        self.config = self.load_config()
+        self.handshake()
+
+        if self.tempcontroller: 
+            self.data_monitoring_refresh_s = 1
+            self.data_logging_cycle_s = 20
+            self.tempcontroller.start_logging(refresh_s = self.data_monitoring_refresh_s)
+
+        self.liveplot_tempcontroller()
+
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return 
+    
+    def close(self):
+        if self.tempcontroller:
+            self.tempcontroller.close()
+            self.tempcontroller = None
+        if self.liveplotter:
+            self.liveplotter.close()
+            self.liveplotter = None
+
+        self.__exit__(None, None, None)
+        return
+
+    def load_config(self):
+        config_path = os.path.join(self.config_dir, 'config.json')
+        with open(config_path, 'r') as f:
+            return json.load(f)
+
+    def handshake(self):
+        
+        print("Trying to establish driver instantiations...")
+
+        try:
+            self.liveplotter: LivePlotAgent = LivePlotAgent()
+            self.liveplot_refresh_rate = self.config["liveplotter"]["refresh_rate"]
+
+            self.tempcontroller: TempControl_CTC100 = TempControl_CTC100(**self.config["tempcontroller"])
+
+            self.log_dir = self.config["logging"]["relative_dir"]
+
+            print("Driver instantiation successful!!!.")
+            return 
+
+        except Exception as e:
+            print(f"Error during driver instantiation: {e}")
+
+    def liveplot_tempcontroller(self):
+
+        no_plots = 5
+        
+        def get_data():
+            return self.tempcontroller.data[:no_plots]
+
+
+        plot_args ={
+            'refresh_interval': self.liveplot_refresh_rate,
+            'title': "Live Temp Controller Data",
+            'xlabel': "Time (s)",
+            'ylabel': "Temperature (°K)",
+            'no_plots': no_plots,
+            'plot_labels': self.tempcontroller.data_names[:no_plots],
+        }
+
+        self.liveplotter.new_liveplot(data_func = get_data, kill_func = None, **plot_args)
+
+        return 
+
+
+
+
+if __name__ == '__main__':
+    with CryoCycler()as cc:
+        print("\n>>>> USE CRYOCYCLER OBJECT AS cc <<<<\n")
+        import code; code.interact(local=locals())
--- drivers/tempcontroller_ctc100.py
+++ drivers/tempcontroller_ctc100.py
@@ -1,21 +1,31 @@
+#%%
 import os
 import sys
 import threading
+import numpy as np
+import time
 
 from generic_instrument_dependencies.generic_instrument import GenericInstrument
 
 
+#%%
+
 class TempControl_CTC100(GenericInstrument):
-    def __init__(self, address, name):
+    def __init__(self, address, name, baud_rate = 9600):
         self.write_term = '\n'
         self.read_term = '\r\n'
         super().__init__(address, name = name,
                         write_term = self.write_term, 
                         read_term = self.read_term)
         
-        self.baud_rate = 9600
+        self.baud_rate = baud_rate
         self.client.baud_rate = self.baud_rate
 
+        self.data_length = 100
+        self.data_names = self.get_data("names")
+        self.data = np.zeros((len(self.data_names), self.data_length))
+
+        self.is_monitoring = False
 
     def __enter__(self):
         return self
@@ -24,6 +34,8 @@ class TempControl_CTC100(GenericInstrument):
         return
     
     def close(self):
+        self.stop_logging()
+        time.sleep(2)
         self.client.close()
         self.client = None
         self.__exit__(None, None, None)
@@ -35,22 +47,51 @@ class TempControl_CTC100(GenericInstrument):
     def get_output(self):
         return self.query("getOutput?")
 
-    def populate_data_from_tc(self, type = 'values'):
-        """ Collects data from temp controller and puts it in traits """
+    def get_data(self, type = 'values'):
+        """ Collects data from temp controller and updates dictionary """
         if type == 'values':
-            tclist = self.getoutput()  
-            tclist = [tclist] # make into list of list
-
+            output = self.get_output()  
+            output = [float(x) for x in output.replace(' ','').split(',')]
             
         elif type == 'names':
-            tclist = self.tempcontroller.getoutputnames()
-            tclist = tclist.replace(' ','').split(',')
-            tclist = [tclist] # make into list of list
+            output = self.get_outputnames()
+            output = [x for x in output.replace(' ','').split(',')]
 
+        return output
 
+    def __data_loop__(self, refresh_s = 1.0):
+        while self.is_monitoring:
+            try:
+                new_data = self.get_data("values") 
+                if self.data.shape[1] < self.data_length:
+                    self.data = np.column_stack((self.data, new_data))
 
+                else: 
+                    self.data = np.roll(self.data, -1, axis=1)
+                    self.data[:,-1] = new_data
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                print("Stopping data update loop")
+                break
+            
+            time.sleep(refresh_s)
+        self.is_monitoring = False
+        return
+
+    def start_logging(self, refresh_s = 1.0):
+        self.is_monitoring = True
+        self.monitoring_thread = threading.Thread(target=self.__data_loop__, args=(refresh_s,), daemon=True)
+        self.monitoring_thread.start()
+        return 
+    
+    def stop_logging(self):
+        self.is_monitoring = False
+        if self.monitoring_thread:
+            self.monitoring_thread.join()
+        return
 
 if __name__ == '__main__':
-    with TempControl_CTC100('GPIB::1::INSTR', 'CTC100') as temp_controller:
+    with TempControl_CTC100('ASRL/dev/ttyUSB0::INSTR', 'CTC100') as tc:
         print("Connected to CTC100 interactive window session")
+        print("\n>>>> USE TEMP CONTROLLER OBJECT AS tc <<<<\n")
         import code; code.interact(local=locals())
